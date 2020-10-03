6208ea40f2255f490dac5c1804c57dd73cfc5e39
Leonardo <leonardogoncalvesp@yahoo.com>
2020-10-03 05:53:55 +0100

    Established FizzBuzz norms.

--- src/test/java/test/FizzBuzz_.java
+++ src/test/java/test/FizzBuzz_.java
@@ -18,16 +18,26 @@ public class FizzBuzz_ {
     public void should_return_number_when_it_is_not_fizz_and_not_buzz() {
         assertThat(fizzBuzz.of(1)).isEqualTo("1");
         assertThat(fizzBuzz.of(2)).isEqualTo("2");
+
     }
 
     @Test
-    public void should_return_Fizz_when_it_is_3() {
+    public void should_return_Fizz_when_it_is_multiple_of_3() {
         assertThat(fizzBuzz.of(3)).isEqualTo("Fizz");
+        assertThat(fizzBuzz.of(6)).isEqualTo("Fizz");
+    }
+
+    @Test
+    public void should_return_Fizz_when_it_is_multiple_of_5() {
+        assertThat(fizzBuzz.of(5)).isEqualTo("Buzz");
+
     }
 
     private class FizzBuzz {
         public String of(int number) {
-            return number == 3 ? "Fizz" : String.valueOf(number);
+            if (number % 3 == 0) return "Fizz";
+            if (number % 5 == 0) return "Buzz";
+            return String.valueOf(number);
         }
     }
 }
