4a5af22afac9888ae7153ee31375a53faa20f397
Leonardo <leonardogoncalvesp@yahoo.com>
2020-10-03 05:02:10 +0100

    Use of test setup via @Before tag

--- src/test/java/test/FizzBuzz_.java
+++ src/test/java/test/FizzBuzz_.java
@@ -1,15 +1,21 @@
 package test;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class FizzBuzz_ {
     //1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 Fizz Buzz 16 17
+    FizzBuzz fizzBuzz = new FizzBuzz();
+
+    @Before
+    public void setUp() {
+        fizzBuzz = new FizzBuzz();
+    }
 
     @Test
     public void should_return_number_when_it_is_not_fizz_and_not_buzz() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("1", fizzBuzz.get(1));
     }
 
