ab75209c8e8b417c7232be166aa99fc24ee34495
Leonardo <leonardogoncalvesp@yahoo.com>
2020-10-03 04:58:29 +0100

    Naming conventions (snake_case)

--- src/test/java/test/FizzBuzz_.java
+++ src/test/java/test/FizzBuzz_.java
@@ -8,7 +8,7 @@ public class FizzBuzz_ {
     //1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 Fizz Buzz 16 17
 
     @Test
-    public void name() {
+    public void should_return_number_when_it_is_not_fizz_and_not_buzz() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("1", fizzBuzz.get(1));
     }
