c298ec2fd2267501a82ac3839962539676c114cb
Leonardo <leonardogoncalvesp@yahoo.com>
2020-10-03 05:47:44 +0100

    Creation of second test

--- src/test/java/test/FizzBuzz_.java
+++ src/test/java/test/FizzBuzz_.java
@@ -3,7 +3,7 @@ package test;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzz_ {
     //1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 Fizz Buzz 16 17
@@ -16,12 +16,18 @@ public class FizzBuzz_ {
 
     @Test
     public void should_return_number_when_it_is_not_fizz_and_not_buzz() {
-        assertEquals("1", fizzBuzz.get(1));
+        assertThat(fizzBuzz.of(1)).isEqualTo("1");
+        assertThat(fizzBuzz.of(2)).isEqualTo("2");
+    }
+
+    @Test
+    public void should_return_Fizz_when_it_is_3() {
+        assertThat(fizzBuzz.of(3)).isEqualTo("Fizz");
     }
 
     private class FizzBuzz {
-        public String get(int number) {
-            return "1";
+        public String of(int number) {
+            return number == 3 ? "Fizz" : String.valueOf(number);
         }
     }
 }
